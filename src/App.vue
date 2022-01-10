9c912395dbbe263fd9adadd4ad51fcbe7521cb42
BuildTools <unconfigured@null.spigotmc.org>
2022-01-10 15:47:21 +0100

    First

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+
+      <v-toolbar-title>Weather | La météo de Demain !</v-toolbar-title>
+
+    </v-app-bar>
+
+    <v-main>
+      <Weather/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Weather from './components/Weather.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    Weather,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
